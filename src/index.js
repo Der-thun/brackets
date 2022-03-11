f030f0ef1bf44559be745997a19e6eca87da6b3b
Boris Kortunov <boris.kortunov@gmail.com>
2022-03-11 20:11:42 +0200

    feat: complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	if (str === '') return true;
+  if (str.length % 2 === 1) return false;	
+  let count = 1;
+  while(count !== 0) {
+    count = 0;
+    bracketsConfig.forEach(elem => {
+      let block = elem[0] + elem[1];      
+      if (str.indexOf(block) !== -1) {    	
+        count++;
+        let a = str.indexOf(block);
+        str = str.slice(0, a) + str.slice(a + 2);
+      }
+    });
+  }
+  if (str.length === 0) return true;
+  return false;
 }
